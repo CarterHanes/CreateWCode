84b7dcc8f2d83bbfcc67ac0211f3d92527ebc1b6
CarterHanes <25hanec@elida.k12.oh.us>
2022-09-07 12:10:19 -0400

    Finished everything, turns based on input

--- Prototype 1 Car/Assets/Scripts/PlayerController.cs
+++ Prototype 1 Car/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-  
+
 {
-    public float speed = 20.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 120.0f;
+    private  float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame updateg
     void Start()
     {
@@ -18,11 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
-        //Move the Vehicle Forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input
+        forwardInput = Input.GetAxis("Vertical");
+        //Moves car based on Vertical Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       //Rotates car based on Horizontal Input 1
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+       
+       
     }
 }
